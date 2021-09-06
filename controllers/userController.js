4c39f00f6153bbf20fd32a3f8672df5f013d87fd
Sabina <sabina.vds@gmail.com>
2021-09-06 14:17:24 +0200

    init code

---
+++ controllers/userController.js
@@ -0,0 +1,24 @@
+
+const User = require('../models/User')
+
+
+exports.login = function() {
+
+}
+
+exports.logout = function() {
+    
+}
+
+exports.register = function(req, res) {
+    let user = new User(req.body)
+    user.register()
+    res.send('thank you for trying to register')
+    
+}
+
+exports.home = function(req, res) {
+    res.render('home-guest')
+    
+}
+
---
+++ models/User.js
@@ -0,0 +1,15 @@
+//uppercase User is the blueprint for model//
+
+let User = function(data) {
+//body of the constructor function//
+this.formData = data
+
+//user is the this in this case//
+}
+
+User.prototype.register = function() {
+    
+
+}
+
+module.exports = User
